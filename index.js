f0165434464480ef26efac60c2c0838c0bb016d5
mhmdabed11 <Mhmdabed11>
2020-08-12 20:59:49 +0300

    update

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const fs = require("fs");
 const path = require("path");
 const { spawn } = require("child_process");
 const { Toolkit } = require("actions-toolkit");
+const md = require("markdown-it")({
+  html: true,
+  linkify: true,
+  breaks: true
+});
 
 const exec = (cmd, args = []) =>
   new Promise((resolve, reject) => {
@@ -30,7 +35,7 @@ Toolkit.run(
   async tools => {
     const readmeContent = fs.readFileSync("./README.md", "utf-8").split("\n");
     const index = readmeContent.findIndex(entry => entry === "<!--TWITTER_STATS_START-->");
-    readmeContent[index + 1] = "## This is twitter stats";
+    readmeContent[index + 1] = md.render("## These are my twitter stats");
     fs.writeFileSync("./README.md", readmeContent.join("\n"));
     await commitFile();
     tools.exit.success("Updated ");
