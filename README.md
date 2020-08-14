35b71add844fef63b35e560c791a13deea479d12
mhmdabed11 <Mhmdabed11>
2020-08-14 12:04:53 +0300

    update

--- README.md
+++ README.md
@@ -9,17 +9,9 @@ This action prints "Hello World" or "Hello" + the name of a person to greet to t
 **Required** The name of the person to greet. Default `"World"`.
 
 ## Outputs
-<!--TWITTER_STATS_START-->
-## This is twitter stats
-<!--TWITTER_STATS_END-->
 
 ### `time`
 
 The time we greeted you.
 
 ## Example usage
-
-uses: actions/hello-world-javascript-action@v1
-with:
-who-to-greet: 'Mona the Octocat'
-
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const core = require("@actions/core");
 const fs = require("fs");
-const path = require("path");
 const { spawn } = require("child_process");
 const { Toolkit } = require("actions-toolkit");
+const core = require("@actions/core");
+const axios = require("axios");
 const md = require("markdown-it")({
   html: true,
   linkify: true,
   breaks: true
 });
 
+/**
+ * Execute shell commands
+ * @param {stringt} cmd
+ * @param {array} args
+ */
 const exec = (cmd, args = []) =>
   new Promise((resolve, reject) => {
     const app = spawn(cmd, args, { stdio: "inherit" });
@@ -23,6 +28,9 @@ const exec = (cmd, args = []) =>
     app.on("error", reject);
   });
 
+/**
+ * Commit changes to github
+ */
 const commitFile = async () => {
   await exec("git", ["config", "--global", "user.email", "mohammad_aabed@hotmail.com"]);
   await exec("git", ["config", "--global", "user.name", "mhmdabed11"]);
@@ -31,14 +39,58 @@ const commitFile = async () => {
   await exec("git", ["push"]);
 };
 
+function getLatestTweets(username, count) {
+  return axios.get(
+    `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${username}&count=${count}&exclude_replies=true`,
+    {
+      headers: {
+        Authorization:
+          "Bearer AAAAAAAAAAAAAAAAAAAAAK0PGwEAAAAAtnxlgULEI2FJmfu2DLLv4sq7tSY%3DI1Nmq5rOdGafTOu7zuwP0NMZUnx9mnCVIHszOex2epyx9gnj9M"
+      }
+    }
+  );
+}
+
 Toolkit.run(
   async tools => {
-    const readmeContent = fs.readFileSync("./README.md", "utf-8").split("\n");
-    const index = readmeContent.findIndex(entry => entry === "<!--TWITTER_STATS_START-->");
-    readmeContent[index + 1] = md.render("## These are my twitter stats");
-    fs.writeFileSync("./README.md", readmeContent.join("\n"));
+    const twitterUserName = core.getInput("USERNAME");
+    const numberOfTweets = core.getInput("NUMBER_OF_TWEETS");
+
+    getLatestTweets(twitterUserName, numberOfTweets).then(res => {
+      tweets = res.data.map(tweet => ({
+        text: tweet.text,
+        url: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+      }));
+      let readmeContent = fs.readFileSync("./README.md", "utf-8").split("\n");
+      const startIndex = readmeContent.findIndex(entry => entry === "<!--TWITTER_STATS_START-->");
+      const endIndex = readmeContent.findIndex(entry => entry === "<!--TWITTER_STATS_END-->");
+      latestTweets = tweets.reduce(
+        (list, tweet) => list + `- 🐤 [${tweet.text.split("\n").join(" ")}](${tweet.url})\n`,
+        ""
+      );
+      if (endIndex - startIndex === 1) {
+        readmeContent = [
+          ...readmeContent.slice(0, endIndex),
+          md.render(latestTweets).trim(),
+          ...readmeContent.slice(endIndex)
+        ];
+      } else {
+        readmeContent = [
+          ...readmeContent.slice(0, startIndex + 1),
+          ...readmeContent.slice(endIndex)
+        ];
+        const tempEndIndex = readmeContent.findIndex(entry => entry === "<!--TWITTER_STATS_END-->");
+        readmeContent = [
+          ...readmeContent.slice(0, tempEndIndex),
+          md.render(latestTweets).trim(),
+          ...readmeContent.slice(tempEndIndex)
+        ];
+      }
+
+      fs.writeFileSync("./README.md", readmeContent.join("\n"));
+    });
     await commitFile();
-    tools.exit.success("Updated ");
+    tools.exit.success("Updated");
   },
   {
     event: ["schedule", "workflow_dispatch", "push"]
